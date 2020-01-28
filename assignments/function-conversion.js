5a2f213b161b1e4116d868ba47156b12b71b7cf0
barbaro reyes <barbaroreyes@barbaros-iMac.attlocal.net>
2020-01-27 18:43:34 -0600

    save

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,19 +1,14 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {
-// console.log("Function was invoked!");
-// };
-// myFunction();
+let myFunction =  () => "Function was invoked!";
+console.log(myFunction());
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
-// anotherFunction("Example");
+let anotherFunction = param => param ;
+console.log(anotherFunction("Example")) 
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
+let add = (param1, param2) => param1 + param2;
+
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
